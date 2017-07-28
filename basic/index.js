7c3d3f06d1c8023423ca8453e6f620d4b75e5a38
psfrankie <frankie-orourke@pluralsight.com>
2017-07-27 20:02:52 -0700

    Initial Commit

---
+++ basic/index.js
@@ -0,0 +1,40 @@
+const { call, buildFunctions } = require('effects-as-data')
+const fetch = require('isomorphic-fetch')
+
+function httpGetCommand(url) {
+  return {
+    type: 'httpGet',
+    url
+  }
+}
+
+function httpGetHandler(cmd) {
+  return fetch(cmd.url).then(r => r.json())
+}
+
+function* getPeople() {
+  const { results } = yield httpGetCommand('https://swapi.co/api/people')
+  const names = results.map(p => p.name)
+  return names
+}
+
+const config = {
+  onCommandComplete: telemetry => {
+    console.log('Telemetry (from onCommandComplete):', telemetry)
+  }
+}
+
+const functions = buildFunctions(
+  config,
+  { httpGet: httpGetHandler },
+  { getPeople }
+)
+
+functions
+  .getPeople()
+  .then(names => {
+    console.log('\n')
+    console.log('Function Results:')
+    console.log(names.join(', '))
+  })
+  .catch(console.error)
